c512056213d60638e4808d91ba053ef7ed4af910
k920049 <k920049@naver.com>
2017-05-29 16:24:03 +0900

    Resolved issue related to the batches of images

--- python/SIGN_main.py
+++ python/SIGN_main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from SIGN_file_reader import SIGN_file_reader
+from PIL import Image
 
 
 def main():
@@ -28,15 +29,19 @@ def main():
         tep_key, tep_value = test_pos_reader.read(test_pos_producer)
         ten_key, ten_value = test_neg_reader.read(test_neg_producer)
 
-        tp_image = tf.image.decode_jpeg(tp_value)
-        tn_image = tf.image.decode_jpeg(tn_value)
-        tep_image = tf.image.decode_jpeg(tep_value)
-        ten_image = tf.image.decode_jpeg(ten_value)
+        tp_image = tf.image.decode_jpeg(tp_value, channels=1)
+        tp_image = tf.image.resize_images(tp_image, size=[255, 255])
+        tn_image = tf.image.decode_jpeg(tn_value, channels=1)
+        tn_image = tf.image.resize_images(tn_image, size=[255, 255])
+        tep_image = tf.image.decode_jpeg(tep_value, channels=1)
+        tep_image = tf.image.resize_images(tep_image, size=[255, 255])
+        ten_image = tf.image.decode_jpeg(ten_value, channels=1)
+        ten_image = tf.image.resize_images(ten_image, size=[255, 255])
 
-        tp_label = tf.ones([train_pos_size])
-        tn_label = tf.zeros([train_neg_size])
-        tep_label = tf.ones([test_pos_size])
-        ten_label = tf.zeros([test_neg_size])
+        tp_label = tf.ones([1])
+        tn_label = tf.zeros([1])
+        tep_label = tf.ones([1])
+        ten_label = tf.zeros([1])
 
         train_x_pos_batch, train_y_pos_batch = \
             tf.train.batch([tp_image[0: -1], tp_label[0: -1]], batch_size=128)
@@ -51,11 +56,14 @@ def main():
 
     init = tf.global_variables_initializer()
 
+
     with tf.Session() as sess:
         sess.run(init)
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
+        # 128 images size [254, 255, 1]
+        x_batch, y_batch = sess.run([train_x_pos_batch, train_y_pos_batch])
 
         coord.request_stop()
         coord.join(threads)
