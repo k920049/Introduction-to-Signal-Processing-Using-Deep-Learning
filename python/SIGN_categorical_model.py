9ca6a90e3a93dfc0282890acd291fdd0b93f5663
k920049 <k920049@naver.com>
2017-06-06 22:55:15 +0900

    Finished implementing the architecture

--- python/SIGN_categorical_model.py
+++ python/SIGN_categorical_model.py
@@ -2,22 +2,70 @@ import tensorflow as tf
 import numpy as np
 
 # constants
-X_dim = 255 * 255
+X_dim = 256
 Y_dim = 2
-Z_dim = 64
-value_lambda = 1.0
+Z_dim = 256 * 256
+value_lambda = 0.001
 
-X = tf.placeholder(tf.float32, shape=[None, X_dim])
+X = tf.placeholder(tf.float32, shape=[None, X_dim, X_dim, 1])
 Y = tf.placeholder(tf.float32, shape=[None, Y_dim])
 Z = tf.placeholder(tf.float32, shape=[None, Z_dim])
 
 initializer = tf.contrib.layers.variance_scaling_initializer
 activation_function = tf.nn.elu
 
+custom_filter = np.ones(shape=[128, 256, 256, 1], dtype=np.float)
+custom_filter[:, 255, :, :] = 0
+custom_filter[:, :, 255, :] = 0
+
+custom_filter = tf.constant(custom_filter, dtype=tf.float32)
+
 
 def discriminator(x, name=None):
     with tf.name_scope(name, "discriminator", [x]) as scope:
 
+        D_conv_1 = tf.layers.conv2d(inputs=x, filters=16, kernel_size=[
+                                    5, 5], padding='SAME', activation=activation_function)
+        # [256, 256]
+        D_mean_pool_1 = tf.nn.pool(D_conv_1, window_shape=[
+                                   2, 2], pooling_type='AVG', padding='VALID', strides=[2, 2])
+        # [128, 128]
+        D_conv_2 = tf.layers.conv2d(D_mean_pool_1, filters=32, kernel_size=[
+                                    3, 3], padding='SAME', activation=activation_function)
+        # [128, 128]
+        D_mean_pool_2 = tf.nn.pool(D_conv_2, window_shape=[
+                                   2, 2], pooling_type='AVG', padding='VALID', strides=[2, 2])
+        # [64, 64]
+        D_conv_3 = tf.layers.conv2d(D_mean_pool_2, filters=64, kernel_size=[
+                                    3, 3], padding='SAME', activation=activation_function)
+        # [64, 64]
+        D_mean_pool_3 = tf.nn.pool(D_conv_3, window_shape=[
+                                   2, 2], pooling_type='AVG', padding='VALID', strides=[2, 2])
+        # [32, 32]
+        D_conv_4 = tf.layers.conv2d(D_mean_pool_3, filters=128, kernel_size=[
+                                    3, 3], padding='SAME', activation=activation_function)
+        # [32, 32]
+        D_mean_pool_4 = tf.nn.pool(D_conv_4, window_shape=[
+                                   2, 2], pooling_type='AVG', padding='VALID', strides=[2, 2])
+        # [16, 16]
+        D_conv_5 = tf.layers.conv2d(D_mean_pool_4, filters=256, kernel_size=[
+                                    3, 3], padding='SAME', activation=activation_function)
+        # [16, 16]
+        D_mean_pool_5 = tf.nn.pool(D_conv_5, window_shape=[
+                                   4, 4], pooling_type='AVG', padding='VALID', strides=[4, 4])
+        # [4, 4]
+        D_conv_6 = tf.layers.conv2d(D_mean_pool_5, filters=2, kernel_size=[
+                                    3, 3], padding='SAME', activation=activation_function)
+        # [4, 4]
+        D_mean_pool_6 = tf.nn.pool(D_conv_6, window_shape=[
+                                   4, 4], pooling_type='AVG', padding='VALID', strides=[4, 4])
+        # [1, 1], and finally, [batch_size][1][1][2]
+        D_logit = tf.reshape(D_mean_pool_6, shape=[128, 2])
+        # [batch_size][2]
+
+        return D_logit
+
+        '''
         D_hidden_layer_1 = tf.layers.dense(
             inputs=x, units=255, activation=activation_function)
         D_hidden_layer_2 = tf.layers.dense(
@@ -26,17 +74,43 @@ def discriminator(x, name=None):
                                   activation=activation_function)
 
         return D_logit
+        '''
 
 
 def generator(z, name=None):
     with tf.name_scope(name, "generator", [z]) as scope:
+        # z[128, 4096]
+        input = tf.reshape(z, shape=[128, 256, 256, 1])
+        # input[128, 64, 64, 1]
+        G_conv_1 = tf.layers.conv2d(input, filters=96, kernel_size=[
+                                    8, 8], padding='SAME', activation=activation_function)
+        # [128, 64, 64, 96]
+        # G_upscaled_1 = tf.image.resize_bicubic(images=G_conv_1, size=[128, 128])
+        # [128, 128, 128, 96]
+        G_conv_2 = tf.layers.conv2d(G_conv_1, filters=64, kernel_size=[
+                                    5, 5], padding='SAME', activation=activation_function)
+        # [128, 128, 128, 64]
+        # G_upscaled_2 = tf.image.resize_bicubic(G_conv_2, size=[256, 256])
+        # [128, 256, 256, 64]
+        G_conv_3 = tf.layers.conv2d(G_conv_2, filters=64, kernel_size=[
+                                    5, 5], padding='SAME', activation=activation_function)
+        # [128, 256, 256, 64]
+        G_conv_4 = tf.layers.conv2d(G_conv_3, filters=1, kernel_size=[
+                                    5, 5], padding='SAME', activation=activation_function)
+        # [128, 256, 256, 1]
+        G_logit = G_conv_4 * custom_filter
+        # [128, 256, 256, 1], but filtered out the last column and row
 
+        return G_logit
+
+        '''
         G_hidden_layer_1 = tf.layers.dense(
             inputs=z, units=255, activation=activation_function)
         G_outputs = tf.layers.dense(inputs=G_hidden_layer_1, units=X_dim,
                                     activation=activation_function)
 
         return G_outputs
+        '''
 
 
 with tf.name_scope("training") as scope:
@@ -55,6 +129,7 @@ with tf.name_scope("training") as scope:
         D_entrophy_real = -(D_proba_real * D_log_real)
         D_mean_real = tf.reduce_mean(D_entrophy_real, axis=1)
         D_entrophy_real_mean = tf.reduce_mean(D_mean_real, axis=0)
+        D_entrophy_real_mean = tf.reshape(D_entrophy_real_mean, shape=[1])
 
     with tf.name_scope("category_2") as sub_scope:
         # Gettning Shannon's entrophy in Z's distribution
@@ -62,6 +137,7 @@ with tf.name_scope("training") as scope:
         G_entrophy_fake = -(D_proba_fake * G_log_fake)
         G_mean = tf.reduce_mean(G_entrophy_fake, axis=1)
         G_entrophy_fake_mean = tf.reduce_mean(G_mean, axis=0)
+        G_entrophy_fake_mean = tf.reshape(G_entrophy_fake_mean, shape=[1])
 
     with tf.name_scope("category_3") as sub_scope:
         # Getting Shannon's entrophy between classes
@@ -70,6 +146,7 @@ with tf.name_scope("training") as scope:
 
         G_class_mean = tf.reduce_mean(G_entrophy_fake, axis=0, keep_dims=True)
         G_class = tf.reduce_mean(G_class_mean, axis=1)
+        G_class = tf.reshape(G_class, shape=[1])
 
     with tf.name_scope("supervised") as sub_scope:
         # Getting cross entrophy for labeled data
@@ -78,6 +155,7 @@ with tf.name_scope("training") as scope:
         D_cross_entrophy = -D_cross_entrophy
         D_supervised = tf.reduce_mean(D_cross_entrophy, axis=0)
         D_supervised_weighted = value_lambda * D_supervised
+        D_supervised_weighted = tf.reshape(D_supervised_weighted, shape=[1])
 
     D_loss = D_class - D_entrophy_real_mean + \
         G_entrophy_fake_mean + D_supervised_weighted
--- python/SIGN_main.py
+++ python/SIGN_main.py
@@ -50,6 +50,11 @@ def main():
         test_x_pos_batch = tf.train.batch([tep_image], batch_size=128)
         test_x_neg_batch = tf.train.batch([ten_image], batch_size=128)
 
+    x_dummy = tf.Variable(
+        tf.zeros(shape=[batch_size, 1, 255, 1], dtype=tf.float32))
+    y_dummy = tf.Variable(
+        tf.zeros(shape=[batch_size, 256, 1, 1], dtype=tf.float32))
+
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
@@ -62,9 +67,10 @@ def main():
             # train step
             # positive
             tp_x_batch = sess.run(train_x_pos_batch)
-            x_batch_reshaped = tf.reshape(
-                tensor=tp_x_batch, shape=[128, SIGN_categorical_model.X_dim])
-            x_batch_reshaped = x_batch_reshaped.eval()
+
+            tp_x_batch = tf.concat(values=[tp_x_batch, x_dummy], axis=1)
+            tp_x_batch = tf.concat(values=[tp_x_batch, y_dummy], axis=2)
+            tp_x_batch = tp_x_batch.eval()
 
             Z_sample = sample_Z(batch_size, SIGN_categorical_model.Z_dim)
             Y_label = np.zeros(
@@ -72,22 +78,23 @@ def main():
             Y_label[:, 1] = 1
 
             _, D_loss_current = sess.run([SIGN_categorical_model.D_solver, SIGN_categorical_model.D_loss], feed_dict={
-                                         SIGN_categorical_model.X: x_batch_reshaped, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
+                                         SIGN_categorical_model.X: tp_x_batch, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
 
             _, G_loss_current = sess.run([SIGN_categorical_model.G_solver, SIGN_categorical_model.G_loss], feed_dict={
-                                         SIGN_categorical_model.X: x_batch_reshaped, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
+                                         SIGN_categorical_model.X: tp_x_batch, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
 
             if it % 3 == 0:
                 print("Iteration : {}".format(it))
                 print("Discriminator loss : {:}".format(D_loss_current[0]))
                 print("Generator loss : {:}".format(G_loss_current[0]))
                 print()
-
+            '''
             # negative
             tn_x_batch = sess.run(train_x_neg_batch)
-            x_batch_reshaped = tf.reshape(
-                tensor=tn_x_batch, shape=[128, SIGN_categorical_model.X_dim])
-            x_batch_reshaped = x_batch_reshaped.eval()
+
+            tn_x_batch = tf.concat(values=[tn_x_batch, x_dummy], axis=1)
+            tn_x_batch = tf.concat(values=[tn_x_batch, y_dummy], axis=2)
+            tn_x_batch = tn_x_batch.eval()
 
             Z_sample = sample_Z(batch_size, SIGN_categorical_model.Z_dim)
             Y_label = np.zeros(
@@ -95,10 +102,10 @@ def main():
             Y_label[:, 0] = 1
 
             _, D_loss_current = sess.run([SIGN_categorical_model.D_solver, SIGN_categorical_model.D_loss], feed_dict={
-                                         SIGN_categorical_model.X: x_batch_reshaped, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
+                                         SIGN_categorical_model.X: tn_x_batch, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
 
             _, G_loss_current = sess.run([SIGN_categorical_model.G_solver, SIGN_categorical_model.G_loss], feed_dict={
-                                         SIGN_categorical_model.X: x_batch_reshaped, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
+                                         SIGN_categorical_model.X: tn_x_batch, SIGN_categorical_model.Y: Y_label, SIGN_categorical_model.Z: Z_sample})
 
             if it % 3 == 0:
                 print("Iteration : {}".format(it))
@@ -112,7 +119,7 @@ def main():
 
             # negative
             ten_x_batch = sess.run(test_x_neg_batch)
-
+            '''
         coord.request_stop()
         coord.join(threads)
     '''
